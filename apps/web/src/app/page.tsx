29d55a267e441aa96932b92c963d9f4f5fb9b0a5
shatrughan mishra <shatrughanm485@gmail.com>
2025-12-27 19:52:24 +0530

    setup

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,3 @@
-import TempComp from "@/components/temp-comp";
 import {Button} from "@/components/ui/button"
 
 export default function Home() {
@@ -6,7 +5,6 @@ export default function Home() {
    <div className="flex flex-col h-screen items-center justify-center">
     <h1>Hello World</h1>
     <Button>Click me</Button>
-    <TempComp/>
    </div>
   );
 }
